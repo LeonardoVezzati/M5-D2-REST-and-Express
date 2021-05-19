8bcbae42a37c2b836c657675fd4bc9a9ad945bc6
Leonardo Vezzati <leonardo.vezzati@gmail.com>
2021-05-19 16:53:37 +0200

    authors creation

--- src/authors/index.js
+++ src/authors/index.js
@@ -1,7 +1,30 @@
 import express from "express";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+import { url } from "inspector";
 
+//const __dirname = "";
 const authorRouter = express.Router();
+const filePath = fileURLToPath(import.meta.url);
+const authorsFolderPath = dirname(filePath);
+const authorJSONPath = join(authorsFolderPath, "authors.json");
 
-authorRouter.get();
+authorRouter.post("/", (req, res) => {
+  res.send("");
+});
+authorRouter.get("/", (req, res) => {
+  const content = fs.readFileSync(authorJSONPath);
+  res.send(authorsFolderPath, filePath, url: import.meta.url, authorJSONPath);
+});
+authorRouter.get("/:id", (req, res) => {
+  res.send("");
+});
+authorRouter.put("/:id", (req, res) => {
+  res.send("");
+});
+authorRouter.delete("/:id", (req, res) => {
+  res.send("");
+});
 
 export default authorRouter;
--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import listEndpoints from "express-list-endpoints";
 import authorRouter from "./authors/index";
 
 const server = express();
@@ -6,6 +7,8 @@ const server = express();
 const port = 3001;
 
 server.use("/authors", authorRouter);
+
+console.table(listEndpoints(server));
 
 server.listen(port, () => {
   console.log("the server is listening on port", port);
